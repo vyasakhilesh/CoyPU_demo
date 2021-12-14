43125f3f72dd131323552bad96480d7f7f7639e9
vyasakhilesh <tibvyasa@NB-WXE15342.tib.tibub.de>
2021-12-14 21:38:56 +0100

    upadted docker file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:18.04
+FROM ubuntu:latest
 
 WORKDIR /coypu_demo
 ADD . /coypu_demo
@@ -8,3 +8,8 @@ RUN apt-get --assume-yes upgrade
 RUN apt-get --assume-yes install python3 python3-pip
 RUN pip3 install -r requirements.txt
 CMD tail -f /dev/null
+
+
+# docker build -t coypu_demo:latest .
+# docker tag coypu_demo:latest coypu_demo:1.0
+# docker push coypu_demo:1.0
--- run.sh
+++ run.sh
@@ -5,3 +5,12 @@ dataset_path=''
 python db/csv2dbs.py --host=$host --port=$port --pwd=$pwd --uname=$uname --dbname=$dbname -i=$dataset_path
 
 
+# run Easy RML
+# http://localhost:5000/
+
+# time python -m rdfizer -c ./knowledge_graph/kg_creation/countries/configs/config.ini
+# curl localhost:4000/graph_creation/knowledge_graph/kg_creation/countries/configs/config.ini
+# curl localhost:6000/mapping_transformation/knowledge_graph/kg_creation/countries/configs/config_func_csv.ini
+
+
+
