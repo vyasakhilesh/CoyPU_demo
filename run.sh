ee61b90972bcc5dd5eb6ca83df92490d79e8a9cb
vyasakhilesh <tibvyasa@NB-WXE15342.tib.tibub.de>
2022-01-13 22:01:45 +0100

    run files

--- run.sh
+++ run.sh
@@ -20,8 +20,8 @@ python src/datasets/mysql/csv2dbs.py --host=$host --port=$port --pwd=$pwd --unam
 
 
 #Sdm-rdfizer
-# time python -m rdfizer -c ./knowledge_graph/kg_creation/countries/configs/config.ini
-# curl localhost:4000/graph_creation/knowledge_graph/kg_creation/countries/configs/config.ini
+# time python -m rdfizer -c ./knowledge_graph/kg_creation/concepts/countries/configs/config.ini
+# curl localhost:4000/graph_creation/knowledge_graph/kg_creation/concepts/countries/configs/config.ini
 # docker exec -it coypu_demo_semantic_enrichment python3 -m rdfizer -c /knowledge_graph/kg_creation/concepts/countries/configs/config.ini
 
 
