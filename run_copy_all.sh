681f224746e17c1cae20196d00aeb5f68e7b93c7
vyasakhilesh <tibvyasa@NB-WXE15342.tib.tibub.de>
2022-01-17 15:09:49 +0100

    updated run

--- run_copy_all.sh
+++ run_copy_all.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Copy Grphs data
+echo '#####################Copying Graphs Data##################'
 find data_gen/graphs/concepts/ -name '*.nt' -exec cp -t data_gen/graphs/all_graphs {} +
+
+echo '#####################Copying Shapes Data##################'
 find knowledge_graph/kg_validation/concepts/ -name '*.json' -exec cp -t knowledge_graph/kg_validation/all_shapes {} +
+
+echo '##################### Backing-up Graphs Data on Triple Store ##################'
+ssh node2 'cp /data/coypu/sparql_endpoint/data_load/*.nt /data/coypu/sparql_endpoint/data_backup/'
+
+echo '#####################Copying Graphs Data to Triple Store ##################'
+rsync -avP data_gen/graphs/all_graphs/*.nt node2:/data/coypu/sparql_endpoint/data_load
+
+# for deleting files from remote side which are not on local side
+#rsync -avP --delete data_gen/graphs/all_graphs/*.nt node2:/data/coypu/sparql_endpoint/data_load
+
+
+
 
